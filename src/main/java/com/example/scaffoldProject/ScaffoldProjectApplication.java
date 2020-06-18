caea908a0919cc4f56511ff5fcf6a32a2555e465
Lysander <Lysander@example.com>
2020-06-18 18:57:17 +0800

    init

---
+++ src/main/java/com/example/scaffoldProject/ScaffoldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.scaffoldProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scaffoldProject/ScaffoldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scaffoldProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
